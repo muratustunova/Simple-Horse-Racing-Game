508fc266a7dc8d23e88b576f9fb34fbdbe1dc5c3
BordoKlavyeli <ustunovamurat@gmail.com>
2020-07-22 03:36:37 +0300

    SimpleHorseRacing

---
+++ AtYarisi/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BİR_NUMARA = new System.Windows.Forms.PictureBox();
+            this.İKİ_NUMARA = new System.Windows.Forms.PictureBox();
+            this.ÜÇ_NUMARA = new System.Windows.Forms.PictureBox();
+            this.DÖRT_NUMARA = new System.Windows.Forms.PictureBox();
+            this.BEŞ_NUMARA = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BİR_NUMARA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.İKİ_NUMARA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ÜÇ_NUMARA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DÖRT_NUMARA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BEŞ_NUMARA)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 19;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.button1.Location = new System.Drawing.Point(759, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bahis:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Bakiye: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(387, 20);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(28, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(252, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Oran:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(431, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "x";
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(683, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(519, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(463, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "At Seçin:";
+            // 
+            // BİR_NUMARA
+            // 
+            this.BİR_NUMARA.Image = ((System.Drawing.Image)(resources.GetObject("BİR_NUMARA.Image")));
+            this.BİR_NUMARA.Location = new System.Drawing.Point(13, 54);
+            this.BİR_NUMARA.Name = "BİR_NUMARA";
+            this.BİR_NUMARA.Size = new System.Drawing.Size(139, 91);
+            this.BİR_NUMARA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BİR_NUMARA.TabIndex = 17;
+            this.BİR_NUMARA.TabStop = false;
+            // 
+            // İKİ_NUMARA
+            // 
+            this.İKİ_NUMARA.Image = ((System.Drawing.Image)(resources.GetObject("İKİ_NUMARA.Image")));
+            this.İKİ_NUMARA.Location = new System.Drawing.Point(13, 151);
+            this.İKİ_NUMARA.Name = "İKİ_NUMARA";
+            this.İKİ_NUMARA.Size = new System.Drawing.Size(139, 91);
+            this.İKİ_NUMARA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.İKİ_NUMARA.TabIndex = 18;
+            this.İKİ_NUMARA.TabStop = false;
+            // 
+            // ÜÇ_NUMARA
+            // 
+            this.ÜÇ_NUMARA.Image = ((System.Drawing.Image)(resources.GetObject("ÜÇ_NUMARA.Image")));
+            this.ÜÇ_NUMARA.Location = new System.Drawing.Point(13, 248);
+            this.ÜÇ_NUMARA.Name = "ÜÇ_NUMARA";
+            this.ÜÇ_NUMARA.Size = new System.Drawing.Size(139, 91);
+            this.ÜÇ_NUMARA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ÜÇ_NUMARA.TabIndex = 19;
+            this.ÜÇ_NUMARA.TabStop = false;
+            // 
+            // DÖRT_NUMARA
+            // 
+            this.DÖRT_NUMARA.Image = ((System.Drawing.Image)(resources.GetObject("DÖRT_NUMARA.Image")));
+            this.DÖRT_NUMARA.Location = new System.Drawing.Point(13, 345);
+            this.DÖRT_NUMARA.Name = "DÖRT_NUMARA";
+            this.DÖRT_NUMARA.Size = new System.Drawing.Size(139, 91);
+            this.DÖRT_NUMARA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.DÖRT_NUMARA.TabIndex = 20;
+            this.DÖRT_NUMARA.TabStop = false;
+            // 
+            // BEŞ_NUMARA
+            // 
+            this.BEŞ_NUMARA.Image = ((System.Drawing.Image)(resources.GetObject("BEŞ_NUMARA.Image")));
+            this.BEŞ_NUMARA.Location = new System.Drawing.Point(12, 442);
+            this.BEŞ_NUMARA.Name = "BEŞ_NUMARA";
+            this.BEŞ_NUMARA.Size = new System.Drawing.Size(139, 91);
+            this.BEŞ_NUMARA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BEŞ_NUMARA.TabIndex = 21;
+            this.BEŞ_NUMARA.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.BEŞ_NUMARA);
+            this.panel1.Controls.Add(this.DÖRT_NUMARA);
+            this.panel1.Controls.Add(this.ÜÇ_NUMARA);
+            this.panel1.Controls.Add(this.İKİ_NUMARA);
+            this.panel1.Controls.Add(this.BİR_NUMARA);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1296, 551);
+            this.panel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Yellow;
+            this.label7.Location = new System.Drawing.Point(1264, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 482);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "F   İ  N  İ  S  H";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1296, 545);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BİR_NUMARA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.İKİ_NUMARA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ÜÇ_NUMARA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DÖRT_NUMARA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BEŞ_NUMARA)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox BİR_NUMARA;
+        private System.Windows.Forms.PictureBox İKİ_NUMARA;
+        private System.Windows.Forms.PictureBox ÜÇ_NUMARA;
+        private System.Windows.Forms.PictureBox DÖRT_NUMARA;
+        private System.Windows.Forms.PictureBox BEŞ_NUMARA;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AtYarisi/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            MessageBox.Show("\t\t      ---AT YARIŞI OYUNU--- \n\n Yapacağınız bahis öncelikle kasanızdan eksiltilecek. " +
+                "Eğer yarışı kazanırsanız, daha sonra bahisiniz oynadığınız kat sayı ile çarpılarak kasanıza eklenecektir." +
+                "Eğer Yarışı kaybeder veya yarış bitmeden önce reset tuşuna basarsanız, bahisiniz haliyle boşa gidecektir.");
+            InitializeComponent();
+            textBox2.ReadOnly = true; // textbox readonly özelliği aktifleştirdim.
+            numericUpDown1.Value = 2; // numericUpDown başlangıç değeri 2 yapıldı. 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            /*Form açılırken comboBox'a atların üzerlerindeki numara göre isimler atadım. 
+             kullanıcı için yapılan bir atamadır. */
+            comboBox1.Items.Add("1 Numara");
+            comboBox1.Items.Add("2 Numara");
+            comboBox1.Items.Add("3 Numara");
+            comboBox1.Items.Add("4 Numara");
+            comboBox1.Items.Add("5 Numara");
+            //başlangıçta verilecek olan bakiye miktarını tanımladım.
+            textBox2.Text = "1000";
+            //Başlat butonuna basıldığında textbox1 boş ise uyarı vermeli.
+            textBox1.Text =" ";
+            //Benim manuel ayarladığım Form boyutunu kullanıcı değiştiremesin diye getirdiğim kısıtlamalar. 
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            atlarındurumu(false);  // atlar hareket etmeyecek(gif oynamayacak)
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Kullanıcı minimum 2 den başlayarak ve maksimum 5 e kadar giren oranları kullanabilecek. Getirilen kısıtlama...
+            numericUpDown1.Maximum = 5;
+            numericUpDown1.Minimum = 2;
+            label4.Text = numericUpDown1.Value.ToString();
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*KeyPress kısmında ise textBox1 isim kutucuğumuza sadece int değer girmesini istiyoruz. 
+            Bundan dolayı bu olayı(Event) kullandık.*/
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        /* Bu kısımda atların hareketinin durması, atların konumlarının başlangıç konumuna gelmesi, girilen bahislerin silinmesi,
+         timer1 adlı sayacımın ise durdurulması ayarlandı. Sadece bakiyede kalan para değeri silinmemiştir. */
+       
+        #region Herşeyi resetleme butonu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            numericUpDown1.Value = 2;
+            atlarındurumu(false);
+            timer1.Stop();
+            
+            //atların ilk konumu
+            BİR_NUMARA.Left=12 ;
+            İKİ_NUMARA.Left=12 ;
+            ÜÇ_NUMARA.Left=12;
+            DÖRT_NUMARA.Left =12;
+            BEŞ_NUMARA.Left =12;
+        }
+        #endregion
+
+        // burda overload yaptık.
+        #region Atların hareket etme durumu
+        void atlarındurumu(bool durum, PictureBox at)
+        {
+            BİR_NUMARA.Enabled = İKİ_NUMARA.Enabled 
+            = ÜÇ_NUMARA.Enabled = DÖRT_NUMARA.Enabled
+            = BEŞ_NUMARA.Enabled = durum;
+            at.Enabled = true;
+        }
+        void atlarındurumu (bool durum)
+        {
+            BİR_NUMARA.Enabled = İKİ_NUMARA.Enabled
+            = ÜÇ_NUMARA.Enabled = DÖRT_NUMARA.Enabled
+            = BEŞ_NUMARA.Enabled = durum;
+        }
+        #endregion
+
+        int bakiye, bahis; int Myselectİndex; 
+
+        //yarış başaladığı zaman oluşacak işlemler mevcut.
+        #region başlat butonu 'Click Event' olayı
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != " ")
+            {
+                //Bahis işlemleri
+                int bahis1, bakiye1;
+                bahis1 = Convert.ToInt32(textBox1.Text);
+                bakiye1 = Convert.ToInt32(textBox2.Text);
+
+                if (bahis1 < bakiye1 || bahis1 == bakiye1)
+                {
+                    bakiye1 = bakiye1 - bahis1;
+                    textBox2.Text = bakiye1.ToString();
+                    //etkinleşecek durumlar
+                    bakiye=bakiye1; bahis = bahis1;  
+                    atlarındurumu(true);//atlar hareket edecek (gif oynayacak)
+                    timer1.Start();
+                }
+                else if (bahis > bakiye)
+                {
+                    MessageBox.Show("Bakiyeyi aştınız !!!");
+                    textBox1.Text = " ";
+                }
+            }
+                 else if (textBox1.Text == " ")
+                {
+                    MessageBox.Show("bahis girin !");
+                }
+        }
+        #endregion
+
+        /*Timer ı PictureBox'ları hareket ettirmeki için kullandım. "Interval" özelliği ile kaç saniyede ki tekrar etme hızını
+         ayarladım.*/
+        #region timer kullanılan kısım
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            BİR_NUMARA.Left += rand.Next(1, 20);
+            İKİ_NUMARA.Left += rand.Next(1, 20);
+            ÜÇ_NUMARA.Left += rand.Next(1, 20);
+            DÖRT_NUMARA.Left += rand.Next(1, 20);
+            BEŞ_NUMARA.Left += rand.Next(1, 20);
+
+            WinnerHorse(BİR_NUMARA);
+            WinnerHorse(İKİ_NUMARA);
+            WinnerHorse(ÜÇ_NUMARA);
+            WinnerHorse(DÖRT_NUMARA);
+            WinnerHorse(BEŞ_NUMARA);
+        }
+        #endregion
+
+        /* Bu kısımda atların kontrolü ile ilgili ufak algoritmalar yer alıyor. Atların kazanma durumuyla ilgili ufak detaylar
+        içeriyor*/
+        #region Atların kazanmasını kontrol eden void fonksiyonu
+        void WinnerHorse (PictureBox kazanan)
+        {
+            if(kazanan.Right -5 >= label7.Left)
+            {
+                timer1.Stop();
+                atlarındurumu(false);
+                MessageBox.Show("Kazanan :" +kazanan.Name);
+                
+                /*atların eski konumu tekrar manuel girdim. Sebebi ise kazanan at belli olduğunda ve kullanıcı MessageBox'ıı kapattığında
+                 atlar eski yerlerine geçmiyor. Bundan kaynaklı oalrak hemen arkasına ikinci kez başlat butonuna basıldığında ise program hata veriyor.
+                 hata vermesinin sebibi ise atlar zaten FİNİSH çizgisinde fakat tekrar yarışın başlamasını tetiklenmiş oluyor.
+                 Buda aynı atın tekrar kazanmasına ve bakiyeden aynı bahis değerinin düşülmesine sebep oluyor.*/
+                BİR_NUMARA.Left = 12;
+                İKİ_NUMARA.Left = 12;
+                ÜÇ_NUMARA.Left = 12;
+                DÖRT_NUMARA.Left = 12;
+                BEŞ_NUMARA.Left = 12;
+
+                /*comboBox'ın 'Select Index' özelliği ile numericUpDown'ın sayılarının uyuşması, beraber çalışması için 
+                 * bu işlemi yaptım. Çok pratik olmadı farklı bir fonksiyon tanımlayıp çekebilirdim Ancak bir sefer kullandığım için tercih etmedim.*/
+                #region numericUpDown index atama
+                switch (kazanan.Name)
+                {
+                    case "BİR_NUMARA": Myselectİndex = 0; break;
+                    case "İKİ_NUMARA": Myselectİndex = 1; break;
+                    case "ÜÇ_NUMARA": Myselectİndex = 2; break;
+                    case "DÖRT_NUMARA": Myselectİndex = 3; break;
+                    case "BEŞ_NUMARA": Myselectİndex = 4; break;
+                }
+                #endregion
+                
+                /*numericUpDown ile seçilen sayının (1 olsun diyelim) , comboBox'da ki Index değerine ('0' a) eşit olması için bu işlemi yaptım.*/
+                #region bahis işlemleri/Kazanan ata göre değerimizi katlama kısmı
+                //kazanama gerçekleştikten sonra eğer seçilen at kazandıysa para bakiyeye eklenecektir.
+                if (comboBox1.SelectedIndex == Myselectİndex )
+                {
+                    if (numericUpDown1.Value == 2) { bakiye += bahis * 2; }
+                    else if (numericUpDown1.Value == 3) { bakiye += bahis * 3; }
+                    else if (numericUpDown1.Value == 4) { bakiye += bahis * 4; }
+                    else if (numericUpDown1.Value == 5) { bakiye += bahis * 5; }
+                }
+                #endregion
+                //textbox2'ye bakiyede kalan parayı bastırmam gerekiyor.
+                textBox2.Text = bakiye.ToString();
+                if (bakiye == 0)
+                {
+                    MessageBox.Show("Paranız Bitti !!");
+                    Environment.Exit(0);
+                }
+            }
+        }
+        #endregion
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+        }
+    }
+}
